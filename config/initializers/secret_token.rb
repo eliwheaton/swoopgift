57538c90b81e8648c1c853d19025961b5eb1f5bc
Dave West <dave@twosevenzero.com>
2013-08-03 08:25:06 -0400

    New Rails App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Swoopgift::Application.config.secret_key_base = 'be6b64d3f02091b05842278c7485feb1535249293f85373ae4c2773540c6b6cd605fd88bdbd7a8a37b3e75f840312c69cefc5a107493926f8c7d4934eded42b3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Swoopgift::Application.config.session_store :cookie_store, key: '_swoopgift_session'
